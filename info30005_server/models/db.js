c8f03debd297e1be2ac92c8e1856e76f2ebc06b6
Ricky Tanudjaja <rtanudjaja@student.unimelb.edu.au>
2017-04-25 17:31:51 +1000

    connect to hookturns.info

--- info30005_server/models/db.js
+++ info30005_server/models/db.js
@@ -1,6 +1,6 @@
 // Create database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase',function(err){
+mongoose.connect('mongodb://hookturns.info:27017/myappdatabase',function(err){
     if(!err){
         console.log('Connected to mongo');
     }else{
