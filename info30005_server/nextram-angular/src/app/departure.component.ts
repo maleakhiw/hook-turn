2b30d0d6601f6e87371c8fa9859bce04e6ad3b63
krusli <rusli.kenneth@gmail.com>
2017-05-17 17:08:27 +1000

    Styling changes to progress bar

--- info30005_server/nextram-angular/src/app/departure.component.ts
+++ info30005_server/nextram-angular/src/app/departure.component.ts
@@ -12,6 +12,7 @@ import {Component, Input} from '@angular/core';
     <div aria-valuemax="60" aria-valuemin="0" aria-valuenow="40" class="{{'progress-bar progress-bar-' + crowdedness[departure.run_id]?.class.toLowerCase()}}"
         role="progressbar" [ngStyle]="{width: calculateWidth(departure.run_id)}">
       {{crowdedness[departure.run_id]?.class}}
+      <p *ngIf="departure.run_id in crowdedness"></p>
     </div>
   </div>
   `
