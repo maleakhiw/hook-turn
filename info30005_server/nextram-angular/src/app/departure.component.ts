7856f48c72a8e3328575b288570b050a4f27dd7a
krusli <rusli.kenneth@gmail.com>
2017-05-17 17:34:09 +1000

    Move out of progress

--- info30005_server/nextram-angular/src/app/departure.component.ts
+++ info30005_server/nextram-angular/src/app/departure.component.ts
@@ -8,12 +8,11 @@ import {Component, Input} from '@angular/core';
   </div>
 
   <p class="expected">Expected Crowd Level</p>
+  <div *ngIf="!inCrowdedness(departure.run_id)">Data not available yet.</div>
   <div class="progress">
     <div aria-valuemax="60" aria-valuemin="0" aria-valuenow="40" class="{{'progress-bar progress-bar-' + crowdedness[departure.run_id]?.class.toLowerCase()}}"
         role="progressbar" [ngStyle]="{width: calculateWidth(departure.run_id)}">
       {{crowdedness[departure.run_id]?.class}}
-      {{departure.run_id}}
-      <div *ngIf="!inCrowdedness(departure.run_id)">Data not available yet.</div>
     </div>
   </div>
   `
