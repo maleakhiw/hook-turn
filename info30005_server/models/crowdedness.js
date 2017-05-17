ec8245c773996f2ff07af91de6522da04fcfe333
krusli <rusli.kenneth@gmail.com>
2017-05-18 00:50:30 +1000

    Fix syntaxerror

--- info30005_server/models/crowdedness.js
+++ info30005_server/models/crowdedness.js
@@ -5,8 +5,8 @@ var crowdednessSchema = new Schema({
   runID: String,
   stopID: String,
   crowdednessLevel: String,
-  timeReported: {type: Date, default: Date.now(),
-  userID: String }
+  timeReported: {type: Date, default: Date.now() },
+  userID: String
 });
 
 var Crowdedness = mongoose.model("crowdedness", crowdednessSchema);
